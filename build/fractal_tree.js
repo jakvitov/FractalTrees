4b0901d646f3409460566866a096c98276014ca6
Jakub Vítovec <jakvitov@gmail.com>
2023-08-27 09:14:11 +0200

    Input fields for tree attributes added

--- build/fractal_tree.js
+++ build/fractal_tree.js
@@ -1,27 +1,41 @@
 var WIDTH = 700;
 var HEIGHT = 700;
+var ratio = 0.75;
+var iterations = 6;
+//Initial value approx. 2*PI / 11
+var angle = 0.571198664;
+var branchSize = 200;
 var setupCanvas = function () {
     var canvas = document.getElementById("drawBoard");
     var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     return ctx;
 };
-//Translate polar to cartesian. R is len and ang is angle.
+//Because we have the polar angle in (PI-sigma)rad form, our classic formula of 
+//polar to cart is changed a bit
 var polarToCartesian = function (r, ang) {
     return { x: (r * Math.sin(ang)), y: (r * Math.cos(ang)) };
 };
 /**
  * We calculate the next vertex coordinates in polar and translate them into cartesian
- * then we draw the branch
+ * then we draw the branch and call this function to next two branches
 */
 var drawBranch = function (from, state, sigma, length, iterations, limit, context) {
-    var endTrans = polarToCartesian(length, sigma);
-    var end = { x: from.x + endTrans.x, y: from.y + endTrans.y };
-    console.log("Started working on branch: " + from.x + ", " + from.y + ", to: " + end.x + ", " + end.y);
+    //Vector of transformation of the original start point to get the end point
+    console.log("Iteration" + iterations + " Length: " + length);
+    var transformVector = polarToCartesian(length, sigma);
+    var end = { x: from.x + transformVector.x, y: from.y + transformVector.y };
+    //console.log("Started working on branch: " + from.x + ", " + from.y + ", to: " + end.x + ", " + end.y);
     iterations += 1;
+    context.beginPath();
+    context.lineWidth = Math.max(length / 13, 1);
     context.moveTo(from.x, from.y);
     context.lineTo(end.x, end.y);
+    context.stroke();
     if (iterations < limit) {
         //Recursive call for the left and right branches of the tree to come
+        //We move the next angle * the constant angle/2 to both sides for branch rotation
+        //We shorten the next branch by the ratio
         drawBranch(end, state, (sigma + state.theta / 2) % (2 * Math.PI), length * state.ratio, iterations, limit, context);
         drawBranch(end, state, (sigma - state.theta / 2) % (2 * Math.PI), length * state.ratio, iterations, limit, context);
     }
@@ -30,10 +44,22 @@ var drawBranch = function (from, state, sigma, length, iterations, limit, contex
 var drawTree = function () {
     console.log("Started drawing tree.");
     var context = setupCanvas();
+    //We start at the bottom-middle of the screen
     var startCoord = { x: WIDTH / 2, y: HEIGHT };
-    var treeState = { ratio: 0.7, theta: Math.PI / 8 };
-    context.beginPath();
-    drawBranch(startCoord, treeState, Math.PI, 300, 0, 8, context);
-    context.stroke();
+    //State of the tree transformation
+    var treeState = { ratio: ratio, theta: angle };
+    drawBranch(startCoord, treeState, Math.PI, branchSize, 0, iterations, context);
 };
 document.getElementById("drawButton").addEventListener("click", function () { return drawTree(); });
+document.getElementById("iterCount").addEventListener("input", function (ev) {
+    iterations = parseInt(ev.target.value);
+});
+document.getElementById("ratioInput").addEventListener("input", function (ev) {
+    ratio = parseFloat(ev.target.value);
+});
+document.getElementById("angleInput").addEventListener("input", function (ev) {
+    angle = parseFloat(ev.target.value);
+});
+document.getElementById("branchSize").addEventListener("input", function (ev) {
+    branchSize = parseInt(ev.target.value);
+});
--- fractal_tree.ts
+++ fractal_tree.ts
@@ -1,10 +1,21 @@
 const WIDTH : number = 700;
 const HEIGHT : number= 700;
 
+let ratio : number = 0.75;
+let iterations : number = 6;
+//Initial value approx. 2*PI / 11
+let angle : number = 0.571198664;
+let branchSize : number = 200;
+
+/**
+ * We solve the fractal tree problem by recursive way of drawing the branches and transforming the angle and length of them.
+ * We make use of the polar coordinates system to manage the angles and lengths of the next branches.
+ */
+
 interface Coord {
     x : number;
     y : number;
-}
+}   
 
 interface TreeState {
     //Ratio of decrease of branch length - len * ratio = newLen
@@ -13,48 +24,74 @@ interface TreeState {
     theta : number;
 }
 
-const setupCanvas = () => {
+const setupCanvas = () : any => {
     const canvas : any= document.getElementById("drawBoard");
     const ctx : any= canvas.getContext("2d");
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     return ctx; 
 }
 
-//Translate polar to cartesian. R is len and ang is angle.
-const polarToCartesian = (r : number, ang : number) => {
+//Because we have the polar angle in (PI-sigma)rad form, our classic formula of 
+//polar to cart is changed a bit
+const polarToCartesian = (r : number, ang : number) : Coord => {
     return {x : (r * Math.sin(ang)), y : (r*Math.cos(ang))}
 }
 
 /** 
  * We calculate the next vertex coordinates in polar and translate them into cartesian
- * then we draw the branch
+ * then we draw the branch and call this function to next two branches
 */
-const drawBranch = (from : Coord, state : TreeState, sigma : number, length : number, iterations : number, limit : number, context : any) => {
-    let endTrans : Coord = polarToCartesian(length, sigma);
-
-    let end : Coord = {x : from.x + endTrans.x, y : from.y + endTrans.y};
-    console.log("Started working on branch: " + from.x + ", " + from.y + ", to: " + end.x + ", " + end.y);
+const drawBranch = (from : Coord, state : TreeState, sigma : number, length : number, iterations : number, limit : number, context : any) : void => {
+    //Vector of transformation of the original start point to get the end point
+    console.log("Iteration" + iterations + " Length: " + length)
+    let transformVector : Coord = polarToCartesian(length, sigma);
+    let end : Coord = {x : from.x + transformVector.x, y : from.y + transformVector.y};
+    
+    //console.log("Started working on branch: " + from.x + ", " + from.y + ", to: " + end.x + ", " + end.y);
     
     iterations += 1;
 
+    context.beginPath();
+    context.lineWidth = Math.max(length / 13, 1);
     context.moveTo(from.x, from.y);
     context.lineTo(end.x, end.y);
+    context.stroke();
     
     if (iterations < limit){
         //Recursive call for the left and right branches of the tree to come
+        //We move the next angle * the constant angle/2 to both sides for branch rotation
+        //We shorten the next branch by the ratio
         drawBranch(end, state, (sigma + state.theta/2) % (2 * Math.PI), length * state.ratio, iterations, limit, context);
         drawBranch(end, state, (sigma - state.theta/2) % (2 * Math.PI), length * state.ratio, iterations, limit, context);
     }
     return;
 }
 
-const drawTree = () => {
+const drawTree = () : void => {
     console.log("Started drawing tree.");
     const context : any = setupCanvas();
+    //We start at the bottom-middle of the screen
     let startCoord : Coord = { x : WIDTH/2, y : HEIGHT};
-    const treeState : TreeState = {ratio : 0.5, theta : Math.PI/3};
-    context.beginPath();
-    drawBranch(startCoord, treeState, Math.PI, 300, 0, 8, context);
-    context.stroke();
+    
+    //State of the tree transformation
+    const treeState : TreeState = {ratio : ratio, theta : angle};
+    
+    drawBranch(startCoord, treeState, Math.PI, branchSize, 0, iterations, context);
 }
 
 document.getElementById("drawButton").addEventListener("click", () => drawTree());
+document.getElementById("iterCount").addEventListener("input", (ev) => {
+    iterations = parseInt((ev.target as HTMLInputElement).value);
+})
+document.getElementById("ratioInput").addEventListener("input", (ev) => {
+    ratio = parseFloat((ev.target as HTMLInputElement).value);
+})
+
+document.getElementById("angleInput").addEventListener("input", (ev) => {
+    angle = parseFloat((ev.target as HTMLInputElement).value);
+
+})
+
+document.getElementById("branchSize").addEventListener("input", (ev) => {
+    branchSize = parseInt((ev.target as HTMLInputElement).value);
+})
